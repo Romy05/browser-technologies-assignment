d8e0a270a66fd24edab49ad3c19f3582abed3e96
Romy <701224@student.inholland.nl>
2026-02-17 21:32:42 +0100

    Add styling

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Vandaag ben ik begonnen met de opdracht die we hebben gekregen, ik heb onderzoek
 
 Ik heb vooral gewerkt aan de html van de pagina. Dus het ziet er nog erg kaal uit.
 <img src="./public/images/progress1.png" />
+
+## Dinsdag 17-2-2026 Checkout
+Vandaag heb ik gewerkt aan het design van het formulier. Hierbij heb ik met name de website responsive gemaakt en sommige input elementen gestyled. Ook heb ik de NS-kleuren toegepast en de header en de legend sticky gemaakt om de website toegankelijk te maken. 
+
+Volgende keer wil ik de file input beter stylen en wil ik toevoegen dat sommige velden pas openklappen nadat een ander veld is ingevuld. Hierdoor krijgt het formulier hopelijk een betere UX.
+
+Het formulier begint al ergens op te lijken.
+<img src="./public/images/progress2.png" />
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import { seperateCharactersByDot } from "./helpers/string.js";
 
 initInputFields();
+initEventListeners();
 
 function initInputFields() {
     const dateOfDeathField = document.querySelector('#dateOfDeath');
-    /* toIsoString etc gedeelte van stackoverflow: https://stackoverflow.com/a/49916376 */
-    dateOfDeathField.max = new Date().toISOString().split("T")[0];
+    const today = new Date();
+    const minDate = new Date();
+    /* Je hebt 8 maanden om het erfbelastingsformulier in te vullen */
+    minDate.setMonth(today.getMonth() - 8);
+
+     /* toIsoString etc gedeelte van stackoverflow: https://stackoverflow.com/a/49916376 */
+    dateOfDeathField.max = today.toISOString().split("T")[0]; /* Hier zou ik een helper functie van kunnen maken */
+    dateOfDeathField.min = minDate.toISOString().split("T")[0];
 
     const initialsInputs = document.querySelectorAll('[id*="initials"]');
 
@@ -21,3 +28,22 @@ function initInputFields() {
     })
 }
 
+function initEventListeners() {
+    // Source - https://stackoverflow.com/a/74812383 en aangepast met behulp van ChatGPT
+    // Mijn use case is net anders dan die op stackoverflow, omdat de top van mijn element niet 0 is.
+    // Daarom gebruik ik een scroll-listener in plaats van een intersection observer.
+    const el = document.querySelector("#deceased-info-legend");
+
+    function checkPosition() {
+        const rect = el.getBoundingClientRect();
+        const stickyTop = parseFloat(getComputedStyle(el).top);
+
+        if (rect.top <= stickyTop) {
+            el.classList.add("is-pinned");
+        } else {
+            el.classList.remove("is-pinned");
+        }
+    }
+
+    window.addEventListener("scroll", checkPosition);
+}
